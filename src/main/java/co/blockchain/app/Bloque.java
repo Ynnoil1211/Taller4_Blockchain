3db721e5c3988715fed56b3bace211881da8c7ec
Daniel Borre <danisecundarioxd@gmail.com>
2026-09-20 14:33:58 -0500

    feat(Bloque & Blockchain): usar enum en lugar de herencia (simplicidad)

--- src/main/java/co/blockchain/app/Bloque.java
+++ src/main/java/co/blockchain/app/Bloque.java
@@ -5,33 +5,92 @@ LIONNY LIN LI - 0222510050
 MARIA ALEJANDRA RAMOS NAIZIR - 0222510006
  */
 package co.blockchain.app;
+import java.util.Objects;
+public class Bloque {
 
-public abstract class Bloque {
-	int id;
-	String data;
-	String hashPrev;
-	Bloque sig;
-	static int cnt=0;
+    public enum Tipo {
+        ADD,
+        UPDATE,
+        DELETE,
+    }
 
-	public abstract String calcularHash();
-	//Constructor para primer elemento del blockchain
-	public Bloque (){
-		this.hashPrev=null;
-		this.sig=null;
-	}
-	public abstract boolean esValido(Blockchain blockchain);
+    Tipo tipo;
+    int id;
+    String data;
+    String hashPrev;
+    Bloque sig;
+    static int cnt = 0;
 
-	public String getData(){
-	    return data;
-	}
-	public int getId(){
-	    return this.id;
-	}
+    public String calcularHash() {
+        int hashInt = Objects.hash(id, data, hashPrev);
+        return Integer.toHexString(hashInt);
+    }
 
-	public String getHashPrev(){
-		return this.hashPrev;
-	}
-	public String getHash(){
-		return this.calcularHash();
-	}
+    private Bloque(){
+        this.hashPrev = null;
+        this.sig = null;
+    }
+    //Constructor para bloque add
+    static Bloque add(String data) {
+        Bloque bloque = new Bloque();
+        bloque.tipo = Tipo.ADD;
+        bloque.id = cnt++;
+        bloque.data= data;
+        return bloque;
+    }
+    // Constructor para bloque update
+    static Bloque update(int id, String data) {
+        Bloque bloque = new Bloque();
+        bloque.tipo = Tipo.UPDATE;
+        bloque.id = id;
+        bloque.data= data;
+        return bloque;
+    }
+    // Constructor para bloque delete
+    static Bloque delete(int id){
+        Bloque bloque = new Bloque();
+        bloque.tipo = Tipo.DELETE;
+        bloque.id = id;
+        bloque.data = null; // Se resuelve al validar
+        return bloque;
+    }
+
+    boolean esValido(Blockchain blockchain){
+        return switch (this.tipo) {
+            case ADD -> true;
+            case UPDATE -> blockchain.buscarPorId(this.id) != null;
+            case DELETE -> {
+                Bloque orig = blockchain.buscarPorId(this.id);
+                if (orig != null) {
+                    this.data = orig.data;
+                    yield true;
+                }
+                yield false;
+            }
+        };
+    }
+
+    public String getData() {
+        return data;
+    }
+    public String mostrar() {
+        return switch (this.tipo){
+            case ADD -> "[Añadir] id: " + this.id + " - " + this.data;
+            case UPDATE -> "[Actualizar] id: " + this.id + " - " + this.data;
+            case DELETE -> "[Borrar] id: " + this.id + " - " + this.data;
+        };
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getHashPrev() {
+        return this.hashPrev;
+    }
+
+    public String getHash() {
+        return this.calcularHash();
+    }
 }
